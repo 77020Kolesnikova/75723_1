0cdefd12fca4ea4de9440774bc55c2fcc55846b3
77020 <polinakolesnikova371@gmail.com>
2026-04-28 18:03:42 +0300

    Update script.js

--- zad6/script.js
+++ zad6/script.js
@@ -182,22 +182,5 @@ fetch("data.json")
 .then(response => response.json())
 .then(data => loadCVData(data))
 .catch(error => {
-
-    /* якщо fetch не працює */
-    const fallbackData = {
-        skills: [
-            "HTML",
-            "CSS",
-            "JavaScript",
-            "SQL",
-            "Linux"
-        ],
-        projects: [
-            "Strona CV",
-            "Baza danych SQL Server",
-            "Analiza danych Excel"
-        ]
-    };
-
     loadCVData(fallbackData);
 });
